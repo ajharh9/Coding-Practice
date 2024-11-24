f8284e4ea5e82a718f258881834cfbed8faa6153
Azhar Hussain <ajharh999@gmail.com>
2024-11-24 21:46:02 +0530

    Create TwoSum.java

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,17 @@
+// Problem: Two Sum
+// Description: Given an array of integers nums and an integer target,
+// find two numbers such that they add up to a specific target number.
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No solution found");
+    }
+}
